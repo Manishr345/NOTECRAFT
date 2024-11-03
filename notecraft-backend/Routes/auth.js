5af7dd638f6066b54a73036cde5b8ecd45dbdfac
Manishr345 <manishr030405@gmail.com>
2024-11-03 18:13:03 +0530

    Validating whether user already exists or not

--- notecraft-backend/Routes/auth.js
+++ notecraft-backend/Routes/auth.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const User = require('../Models/user');
 
 router.post('/', async (req, res) => {
-    const user = User.create({
+    let user = await User.findOne({email: req.body.email});
+    if(user){
+        return res.status(400).json({error: 'User with this email already exists'});
+    }
+    user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
