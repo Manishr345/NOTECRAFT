53784326c49512ef8acb5ece3335a59484bfb072
Manishr345 <manishr030405@gmail.com>
2024-11-03 18:32:06 +0530

    Implementing express-validator

--- notecraft-backend/Routes/auth.js
+++ notecraft-backend/Routes/auth.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../Models/user');
+const {body, validationResult} = require('express-validator');
 
-router.post('/', async (req, res) => {
+router.post('/', [
+    body('name', 'Name must be of atleast 3 characters').isLength({min: 3}),
+    body('email', 'Please enter a valid email').isEmail(),
+    body('password', 'Password must be of atleast 8 characters').isLength({min: 8})
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({error: errors.array()});
+    }
     let user = await User.findOne({email: req.body.email});
     if(user){
         return res.status(400).json({error: 'User with this email already exists'});
