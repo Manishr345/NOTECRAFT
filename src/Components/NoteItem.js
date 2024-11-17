4f14b6b63f27dee21970c2c51574a2d9b929844c
Manishr345 <manishr030405@gmail.com>
2024-11-17 12:38:54 +0530

    Added style to NoteItem component

--- src/Components/NoteItem.js
+++ src/Components/NoteItem.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 export default function NoteItem(props) {
   return (
-    <div className='m-10 w-[8rem] h-[15rem] bg-pink-400 pl-5 pr-5'>
-      <p>{props.title}</p>
-      <p>{props.description}</p>
-      <p>{props.tag}</p>
+    <div className='m-10 w-[8rem] h-[15rem] bg-pink-400 pl-5 pr-5 rounded-lg border-black border-2'>
+      <p className='font-bold mb-3'>{props.title}</p>
+      <hr/>
+      <p className='mb-3'>{props.description}</p>
+      <hr/>
+      <p className='mt-3'>{props.tag}</p>
     </div>
   )
 }
