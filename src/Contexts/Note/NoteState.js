8a15c908e6e4fb47f2b4d6bebd414b6bc636ee3f
Manishr345 <manishr030405@gmail.com>
2024-11-20 20:39:15 +0530

    Updated addNote function to fetch notes from api

--- src/Contexts/Note/NoteState.js
+++ src/Contexts/Note/NoteState.js
@@ -4,6 +4,20 @@ import { useState } from "react";
 const NoteState = (props) => {
     const [note, setNote] = useState([]);
 
+    const addNote = async (title, description, tag) => {
+      const response = await fetch('http://localhost:5000/api/note/addnotes', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjcyZTEzODIzM2YyNzhlY2Q2OTkxYjJmIn0sImlhdCI6MTczMTA3MzkyMn0.q_B_HK-SzM2n-GdU6ZiJfb4YMD__Lr-fUWQ1atnCexU'
+        },
+        body: JSON.stringify({title, description, tag})
+      });
+      const json = response.json();
+      // setNote(json.note);
+      console.log(json);
+    }
+
     const fetchNotes = async () => {
         const response = await fetch('http://localhost:5000/api/note/fetchnotes', {
           method: 'GET',
@@ -17,7 +31,7 @@ const NoteState = (props) => {
         setNote(json);
       };
     return (
-        <NoteContext.Provider value={{note, setNote, fetchNotes}}>
+        <NoteContext.Provider value={{note, setNote, addNote, fetchNotes}}>
             {props.children};
         </NoteContext.Provider>
     )
